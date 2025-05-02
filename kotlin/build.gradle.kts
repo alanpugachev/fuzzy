a0ba579d1ff74607339ce801aece2276cb538bef
Alan Pugachev <aszneesizzle@gmail.com>
2025-05-03 01:37:41 +0300

    update build.gradle.kts

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -28,9 +28,25 @@ dependencies {
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 
+    /* ktor */
     implementation("io.ktor:ktor-server-html-builder:3.1.2")
     implementation("io.ktor:ktor-server-resources:3.1.2")
+
+    /* serialization */
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
+    /* kafka */
     implementation("org.apache.kafka:kafka-clients:3.6.0")
+
+    /* log4j */
     implementation("org.slf4j:slf4j-simple:2.0.9")
+
+    /* exposed */
+    implementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    runtimeOnly("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-json:0.61.0")
+
+    /* postgresql */
+    implementation("org.postgresql:postgresql:42.7.5")
 }
